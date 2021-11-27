f334fbaf57bf96bd2a321459f59ba25e9b643cd9
pyong <muanda1993@hotmail.com>
2021-11-27 03:24:59 +0000

    setup client side

---
+++ client/pt_tempo/src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <header></header>
+
+      <main  className="container">
+        <div class="angry-grid">
+          <div id="item-0"></div>
+          <div id="item-1">&nbsp;</div>
+          <div id="item-2">&nbsp;</div>
+          <div id="item-3">&nbsp;</div>
+          <div id="item-4">&nbsp;</div>
+        </div>
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
