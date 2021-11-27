4e8b0c80e48cb29b553cbff112bbf71bb25474df
pyong <muanda1993@hotmail.com>
2021-11-27 23:28:14 +0000

    add grid layout to home page

--- client/pt_tempo/src/components/weatherCard.js
+++ client/pt_tempo/src/components/weatherCard.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { BsFillCloudSunFill } from 'react-icons/bs';
+import { BsFillCloudSunFill } from "react-icons/bs";
 import "./weatherCard.css";
-
 
 const WeatherCard = () => {
   return (
@@ -9,7 +8,7 @@ const WeatherCard = () => {
       <div className="top">
         <div className="iconText-section">
           <div className="weatherCard-icon">
-            <BsFillCloudSunFill size={100}/>
+            <BsFillCloudSunFill size={100} color="black" />
           </div>
           <span className="weatherDescription">text</span>
         </div>
@@ -20,10 +19,7 @@ const WeatherCard = () => {
         </div>
       </div>
 
-    <div className="bottom">
-
-    </div>
-
+      <div className="bottom"></div>
     </div>
   );
 };
--- client/pt_tempo/src/pages/home.js
+++ client/pt_tempo/src/pages/home.js
@@ -1,25 +1,110 @@
-import React, { Fragment } from "react"
+import React, { Fragment } from "react";
+import { makeStyles } from "@material-ui/styles";
+import WeatherCard from "../components/weatherCard";
 
+const useStyles = makeStyles({
+  center: {
+    margin: "10% 16%",
+    width: "50%",
+    padding: "10px",
+  },
 
+  angryGrid: {
+    display: "grid",
+    margin: "0 auto",
+    gridTemplateRows:" 1fr 1fr 1fr",
+    gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
+    gap:" 0px",
+    height: "80%",
+  
+    
+  },
+  
+  item0: {
+    backgroundColor:"rgba(255, 248, 248,.2)",
+    gridRowStart: 1,
+    gridColumnStart: 1,
+    gridRowEnd: 2,
+    gridColumnEnd: 2,
+    height: "300px",
+    width: "400px",
+    margin: "10px"
+  },
+  item1: {
+    backgroundColor: "#98A9B5",
+    gridRowStart: 1,
+    gridColumnStart: 2,
+    gridRowEnd: 2,
+    gridColumnEnd: 3,
+    height: "300px",
+    width: "400px",
+    margin: "10px",
+  },
 
-const Home =()=>{
-    return(
-        <Fragment>
-            hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhjj
-            <header></header>
+  item2: {
+    backgroundColor: "#E5C899",
+    gridRowStart: 2,
+    gridColumnStart: 1,
+    gridRowEnd: 3,
+    gridColumnEnd: 2,
+    height: "300px",
+    width: "400px",
+    margin: "10px",   
+  },
 
-<main  className="container">
-  <div class="angry-grid">
-    <div id="item-0"></div>
-    <div id="item-1">&nbsp;</div>
-    <div id="item-2">&nbsp;</div>
-    <div id="item-3">&nbsp;</div>
-    <div id="item-4">&nbsp;</div>
-  </div>
+  item3: {
+  
+    backgroundColor: "#FBE56B",
+    gridRowStart: 2,
+    gridColumnStart: 2,
+    gridRowEnd: 3,
+    gridColumnEnd: 3,
+    height: "300px",
+    width: "400px",
+    margin: "10px",
+    
+  },
+  item4: {
+  
+    backgroundColor: "#FE75DC",
+    gridRowStart: 1,
+    gridColumnStart: 3,
+    gridRowEnd: 3,
+    gridColumnEnd: 4,
+    height: "620px",
+    width: "400px",
+    margin: "10px",
+    
+  }
+  
+});
 
- </main>
-        </Fragment>
-    )
-}
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <Fragment>
+      <main className={classes.center}>
+        <div className={classes.angryGrid}>
+          <div className={classes.item0}>
+            <WeatherCard />
+          </div>
+          <div className={classes.item1}>
+            <WeatherCard />
+          </div>
+          <div className={classes.item2}>
+            <WeatherCard />
+          </div>
+          <div className={classes.item3}>
+            <WeatherCard />
+          </div>
+          <div className={classes.item4}>
+            <WeatherCard />
+          </div>
+        </div>
+      </main>
+    </Fragment>
+  );
+};
 
 export default Home;
